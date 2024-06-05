753f599034257d4548be280a41fbdd21ba13cbfb
darwinhub <dargonzalez81@gmail.com>
2024-06-05 11:48:25 +0200

    .

--- Ejercicio-01-2024/Pago.js
+++ Ejercicio-01-2024/Pago.js
@@ -11,6 +11,20 @@ function processPayment() {
   let comprarEntrada = document.getElementById("comprar-entrada").checked;
 
   // Redirigir a la página de pago
-  window.location.href = "https://example.com/payment?nombre=" + nombre + "&apellido=" + apellido + "&telefono=" + telefono + "&direccion=" + direccion + "&codigoPostal=" + codigoPostal + "&fecha=" + fecha + "&correo=" + correo + "&comprarEntrada=" + comprarEntrada;
+  window.location.href = "https://www.caixabank.es/particular/banca-digital.html?nombre=" + nombre + "&apellido=" + apellido + "&telefono=" + telefono + "&direccion=" + direccion + "&codigoPostal=" + codigoPostal + "&fecha=" + fecha + "&correo=" + correo + "&comprarEntrada=" + comprarEntrada;
 
+}
+
+//Forma de pago -Pasa que si no rellenamos un campo me aparecera una alerta indicando que (Rellena todos los campos del formulario)
+function compra() {
+  let titularTarjeta = document.getElementById("card-holder").value;
+  let numeroTarjeta = document.getElementById("card-number").value;
+  let fechaExpiracion = document.getElementById("expiration-date").value;
+  let cvv = document.getElementById("cvv").value;
+
+  if (titularTarjeta === "" || numeroTarjeta === "" || fechaExpiracion === "" || cvv === "") {
+    alert("Rellena todos los campos del formulario");
+  } else {
+    alert("La compra se ha realizado con éxito");
+  }
 }
